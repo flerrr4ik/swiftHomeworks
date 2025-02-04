1001cd51404a1460aa0c1c23865ffea67666b84f
Andrii Pyrskyi <flerrr4ik@gmail.com>
2025-02-05 01:42:06 +0200

    added project

---
+++ Allert and Git/Allert and Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Allert and Git
+//
+//  Created by admin on 04.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Allert and Git/Allert and GitTests/Allert_and_GitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Allert_and_GitTests.swift
+//  Allert and GitTests
+//
+//  Created by admin on 04.02.2025.
+//
+
+import Testing
+@testable import Allert_and_Git
+
+struct Allert_and_GitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
